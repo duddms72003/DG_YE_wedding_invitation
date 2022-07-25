4e5e14bbcb4735015cd40a1d2646ea5f871cf6ce
duddms72003 <56666270+duddms72003@users.noreply.github.com>
2022-07-25 18:08:29 +0900

    Add files via upload

--- assets/js/common.js
+++ assets/js/common.js
@@ -556,8 +556,8 @@ function copy_to_clipboard(event) {
 
 
 /* 텍스트 스크롤 */
+/*
 let elementsArray = document.querySelectorAll(".scroll-y");
-let container = document.querySelector(".container");
 
 console.log(elementsArray);
 window.addEventListener('scroll', fadeIn );
@@ -573,7 +573,30 @@ function fadeIn() {
   }
 }
 fadeIn();
+*/
 
+
+
+
+function showIt() {
+  const toBeShown = document.querySelectorAll(".scroll-y"); // consider adding :not(.scrolled) to selector to reduce the number of iterations if you don't want to support scrolling up
+
+  // consider taking this outside of the loop and resetting it on window resize to optimize the loop
+  const halfScreen = window.innerHeight / 2;
+
+  toBeShown.forEach((item, i) => {
+    const scrolled = (window.scrollY + window.innerHeight);// - (item.offsetHeight/2);
+
+    if (item.offsetTop - window.scrollY < halfScreen) {
+      item.classList.add('scrolled');
+    } else {
+      item.classList.remove('scrolled');
+    }
+  })
+
+}
+
+/*window.addEventListener('scroll', showIt);*/
 
 
 
